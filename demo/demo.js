eb31343735bb0e089b7558bc033826ed3f79ad14
Paul Ingles <paul@oobaloo.co.uk>
2011-07-13 09:41:46 +0100

    Convert to using libeio for async writing

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,8 @@
 var h = require('../build/default/hdfs.node');
 var hi = new h.Hdfs();
 var data = new Buffer("Hello, my name is Paul", encoding='utf8')
-var writtenBytes = hi.write("/tmp/testfile.txt", data);
-console.log("Wrote " + writtenBytes + " bytes")
+var writtenBytes = hi.write("/tmp/testfile.txt", data, function(bytes) {
+  console.log("Wrote file with " + bytes + ' bytes.');
+});
+console.log("Finished");
+// console.log("Wrote " + writtenBytes + " bytes")
--- src/hdfs.cc
+++ src/hdfs.cc
@@ -11,6 +11,12 @@
 using namespace node;
 using namespace v8;
 
+#define REQ_FUN_ARG(I, VAR)                                             \
+  if (args.Length() <= (I) || !args[I]->IsFunction())                   \
+    return ThrowException(Exception::TypeError(                         \
+                  String::New("Argument " #I " must be a function")));  \
+  Local<Function> VAR = Local<Function>::Cast(args[I]);
+
 class HdfsClient: ObjectWrap
 {
 private:
@@ -49,31 +55,87 @@ public:
     hw->Wrap(args.This());
     return args.This();
   }
+  
+  struct hdfs_write_baton_t {
+    HdfsClient *client;
+    char* filePath;
+    tSize writtenBytes;
+    Persistent<Function> cb;
+    Persistent<Object> buffer;
+  };
 
   static Handle<Value> Write(const Arguments& args)
   {
     HandleScope scope;
     
-    hdfsFS fs = hdfsConnect("default", 0);
+    REQ_FUN_ARG(2, cb);
+
     v8::String::Utf8Value pathStr(args[0]);
     char* writePath = (char *) malloc(strlen(*pathStr) + 1);
     strcpy(writePath, *pathStr);
+
     
-    char* bufData = Buffer::Data(args[1]->ToObject());
-    size_t bufLength = Buffer::Length(args[1]->ToObject());
+    HdfsClient* client = ObjectWrap::Unwrap<HdfsClient>(args.This());
+    
+    hdfs_write_baton_t *baton = new hdfs_write_baton_t();
+    baton->client = client;
+    baton->cb = Persistent<Function>::New(cb);
+    baton->buffer = Persistent<Object>::New(args[1]->ToObject());
+    baton->writtenBytes = 0;
+    baton->filePath = writePath;
+    
+    client->Ref();
+    
+    eio_custom(eio_hdfs_write, EIO_PRI_DEFAULT, eio_after_hdfs_write, baton);
+    uv_ref();
+    
+    return Undefined();    
+  }
+  
+  static int eio_hdfs_write(eio_req *req)
+  {
+    hdfs_write_baton_t *baton = static_cast<hdfs_write_baton_t*>(req->data);
+    char* writePath = baton->filePath;
+    
+    hdfsFS fs = hdfsConnect("default", 0);
+    
+    char* bufData = Buffer::Data(baton->buffer);
+    size_t bufLength = Buffer::Length(baton->buffer);
     
     hdfsFile writeFile = hdfsOpenFile(fs, writePath, O_WRONLY|O_CREAT, 0, 0, 0);
     tSize num_written_bytes = hdfsWrite(fs, writeFile, (void*)bufData, bufLength);
+    
     hdfsFlush(fs, writeFile);
     hdfsCloseFile(fs, writeFile);
     
-    HdfsClient* hw = ObjectWrap::Unwrap<HdfsClient>(args.This());
-    hw->m_count++;
-    Local<Integer> result = Integer::New(num_written_bytes);
+    baton->writtenBytes = num_written_bytes;
 
-    return scope.Close(result);
+    return 0;
   }
+  
+  static int eio_after_hdfs_write(eio_req *req)
+  {
+    HandleScope scope;
+    hdfs_write_baton_t *baton = static_cast<hdfs_write_baton_t*>(req->data);
+    uv_unref();
+    baton->client->Unref();
 
+    Local<Value> argv[1];
+    argv[0] = Integer::New(baton->writtenBytes);
+
+    TryCatch try_catch;
+
+    baton->cb->Call(Context::GetCurrent()->Global(), 1, argv);
+
+    if (try_catch.HasCaught()) {
+      FatalException(try_catch);
+    }
+
+    baton->cb.Dispose();
+
+    delete baton;
+    return 0;
+  }
 };
 
 Persistent<FunctionTemplate> HdfsClient::s_ct;
